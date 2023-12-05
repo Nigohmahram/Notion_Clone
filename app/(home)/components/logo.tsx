95d23de06a0c5bc4d493ea22db35c84c14ec7935
Usmonov Hojiakbar <usmonovmaxsudovich@gmail.com>
2023-12-05 23:12:18 +0900

    Theme Provider

--- app/(home)/components/logo.tsx
+++ app/(home)/components/logo.tsx
@@ -5,6 +5,7 @@ export const Logo = () => {
 	return (
 		<div className='flex items-center gap-x-2'>
 			<Image src={'/logo.svg'} alt='Logo' width={100} height={100} className='object-cover' />
+			<p className='font-semibold text-xl'>Notion</p>
 		</div>
 	);
 };
