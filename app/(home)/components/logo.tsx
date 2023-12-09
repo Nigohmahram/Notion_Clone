79de54d28bb8d772ba3f367b3d052e88e1966cc9
Usmonov Hojiakbar <usmonovmaxsudovich@gmail.com>
2023-12-09 22:39:36 +0900

    Logo ....props

--- app/(home)/components/logo.tsx
+++ app/(home)/components/logo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export const Logo = () => {
 	return (
 		<div className='flex items-center gap-x-2'>
-			<Image src={'/logo.svg'} alt='Logo' width={35} height={35} className='object-cover' />
+			<Image src={'/logo.svg'} alt='Logo0' width={35} height={35} className='object-cover' />
 			<p className='font-semibold text-xl'>Notion</p>
 		</div>
 	);
